b1c6313b7a19b0aef3442b695cb1404b88cde6cd
GeorgeGWL <836248956@qq.com>
2018-09-14 17:13:34 +0800

    6

--- src/main/java/cn/xyzs/api/pojo/MvCommoMenu.java
+++ src/main/java/cn/xyzs/api/pojo/MvCommoMenu.java
@@ -6,17 +6,17 @@ import lombok.Setter;
 import javax.persistence.Column;
 
 public class MvCommoMenu {
-
+    //员工ID
     @Column(name = "USER_ID")
     @Getter
     @Setter
     private String userId;
-
+    //角色ID
     @Column(name = "ROLE_ID")
     @Getter
     @Setter
     private String roleId;
-
+    //
     @Column(name = "COMPO_ID")
     @Getter
     @Setter
