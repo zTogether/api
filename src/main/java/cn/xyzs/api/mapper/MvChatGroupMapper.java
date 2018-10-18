bee8b518c94a6d41a28ba4fbe3bca98b7a8eee1c
GeorgeGWL <836248956@qq.com>
2018-10-18 14:31:32 +0800

    6

---
+++ src/main/java/cn/xyzs/api/mapper/MvChatGroupMapper.java
@@ -0,0 +1,8 @@
+package cn.xyzs.api.mapper;
+
+import cn.xyzs.api.pojo.MvChatGroup;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface MvChatGroupMapper extends Mapper<MvChatGroup> {
+
+}
---
+++ src/main/java/cn/xyzs/api/mapper/MvChatMemberMapper.java
@@ -0,0 +1,8 @@
+package cn.xyzs.api.mapper;
+
+import cn.xyzs.api.pojo.MvChatMember;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface MvChatMemberMapper extends Mapper<MvChatMember> {
+
+}
