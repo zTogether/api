9e8dc2e5720b344dd04ff0b9a169d405bef5e969
GeorgeGWL <836248956@qq.com>
2018-10-21 11:29:59 +0800

    6

--- src/main/java/cn/xyzs/api/controller/PcApiController.java
+++ src/main/java/cn/xyzs/api/controller/PcApiController.java
@@ -21,4 +21,31 @@ public class PcApiController {
         return pcApiService.sendGiftCode(phone,giftCode);
     }
 
+    /**
+     *
+     * @Description: 通知执行员派单成功短信
+     * @author: GeWeiliang
+     * @date: 2018\10\21 0021 11:18
+     * @param: [ctrCode(档案号), gongZhong(工种), gongZhang(工长), tel(工长电话), phone(执行员电话)]
+     * @return: java.util.Map<java.lang.String,java.lang.Object>
+     */
+    @ResponseBody
+    @RequestMapping("/sendPgMsg")
+    public Map<String,Object> sendPgMsg(String ctrCode,String gongZhong,String gongZhang,String tel,String phone){
+        return pcApiService.sendPgMsg(ctrCode,gongZhong,gongZhang,tel,phone);
+    }
+
+    /**
+     *
+     * @Description: 抢单成功提醒短信
+     * @author: GeWeiliang
+     * @date: 2018\10\21 0021 11:28
+     * @param: [ctrCode, date(进场日期), exDir(执行总监), tel(执行总监电话), phone(抢单者电话)]
+     * @return: java.util.Map<java.lang.String,java.lang.Object>
+     */
+    @ResponseBody
+    @RequestMapping("/sendQdMsg")
+    public Map<String,Object> sendQdMsg(String ctrCode,String date,String exDir,String tel,String phone){
+        return pcApiService.sendQdMsg(ctrCode,date,exDir,tel,phone);
+    }
 }
--- src/main/java/cn/xyzs/api/service/PcApiService.java
+++ src/main/java/cn/xyzs/api/service/PcApiService.java
@@ -23,4 +23,48 @@ public class PcApiService {
         return resultMap;
     }
 
+    /**
+     *
+     * @Description: tel:工长电话,phone:执行总监电话
+     * @author: GeWeiliang
+     * @date: 2018\10\21 0021 11:15
+     * @param: [ctrCode, gongZhong, gongZhang, tel, phone]
+     * @return: java.util.Map<java.lang.String,java.lang.Object>
+     */
+    public Map<String,Object> sendPgMsg(String ctrCode,String gongZhong,String gongZhang,String tel,String phone){
+        Map<String,Object> resultMap = new HashMap<>();
+        String code = "500";
+        String msg = "系统异常";
+        String []params = {ctrCode,gongZhong,gongZhang,tel};
+        code = SendMsgUtil.sendMsg("3" , params ,phone);
+        if ("200".equals(code)){
+            msg = "发送成功";
+        }
+        resultMap.put("code",code);
+        resultMap.put("msg",msg);
+        return resultMap;
+    }
+
+    /**
+     *
+     * @Description:
+     * @author: GeWeiliang
+     * @date: 2018\10\21 0021 11:26
+     * @param: [ctrCode, date(进场日期), exDir(执行总监), tel(执行总监电话), phone(抢单者电话)]
+     * @return: java.util.Map<java.lang.String,java.lang.Object>
+     */
+    public Map<String,Object> sendQdMsg(String ctrCode,String date,String exDir,String tel,String phone){
+        Map<String,Object> resultMap = new HashMap<>();
+        String code = "500";
+        String msg = "系统异常";
+        String []params = {ctrCode,date,exDir,tel};
+        code = SendMsgUtil.sendMsg("4" , params ,phone);
+        if ("200".equals(code)){
+            msg = "发送成功";
+        }
+        resultMap.put("code",code);
+        resultMap.put("msg",msg);
+        return resultMap;
+    }
 }
+
