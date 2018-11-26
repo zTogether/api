53738a37db468f9db679319e6a526aa0729507bb
GeorgeGWL <836248956@qq.com>
2018-11-26 10:32:24 +0800

    23

---
+++ src/main/java/cn/xyzs/api/mapper/XyStarEvaListMapper.java
@@ -0,0 +1,21 @@
+package cn.xyzs.api.mapper;
+
+import cn.xyzs.api.pojo.XyStarEvaMain;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import tk.mybatis.mapper.common.Mapper;
+
+import java.sql.SQLException;
+
+public interface XyStarEvaListMapper extends Mapper<XyStarEvaMain> {
+
+    @Insert("<script>" +
+            "INSERT INTO XY_STAR_EVA_LIST(EVA_NO,LEVEL,EVALUATION,EVA_NAME,EVA_TYPE,EVA_DATE) " +
+            "VALUES(#{evaNo,jdbcType=VARCHAR},#{level,jdbcType=VARCHAR},#{evaluation,jdbcType=VARCHAR},#{evaName,jdbcType=VARCHAR}," +
+            "#{evaType,jdbcType=VARCHAR},SYSDATE) " +
+            "</script>")
+    public void addEvaList(@Param("evaNo") String evaNo,@Param("level") String level,
+                           @Param("evaluation") String evaluation,@Param("evaName") String evaName,@Param("evaType") String evaType) throws SQLException;
+
+}
